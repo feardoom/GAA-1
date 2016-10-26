b0482014772cf93204a4d4a685ea48c0ae669b31
Trainerpl135 <Trainerpl135@SLFRPL135PCFORM.ensino.atec.local>
2016-10-26 11:33:29 +0100

    Calculadora balanço

--- src/Principal.java
+++ src/Principal.java
@@ -8,6 +8,23 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ola Turma!");
+		
+		int quotas 			= 300,
+				recBaile 	= 200,
+				recBar		= 250;
+		
+		int totalReceitas = quotas + recBaile + recBar;
+		
+		int agua 		= 80,
+			luz 		= 60,
+			telefone	= 70;
+		
+		int despesas = agua + luz + telefone;
+		
+		int balanco = totalReceitas - despesas;
+		
+		System.out.println("Angariou " + totalReceitas + "€");
+		System.out.println("Gastou " + despesas + "€");
+		System.out.println("O balanço é de " + balanco + "€");
 	}
-
 }
