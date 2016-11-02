e4c3c2c9628cbbad3eac501fcbb2d593cac84b5c
Trainerpl135 <Trainerpl135@SLFRPL135PCFORM.ensino.atec.local>
2016-11-02 12:12:51 +0000

    aula 021116 - final

---
+++ src/dia021116/CalculadoraInfinita.java
@@ -0,0 +1,16 @@
+package dia021116;
+
+public class CalculadoraInfinita {
+
+	public CalculadoraInfinita() {
+		/*
+		 * A calculadora deve:
+		 * 1) Pedir o primeiro número;
+		 * 2) Pedir o segundo número; 
+		 * 3) Pedir a operação; 
+		 * 4) Apresentar o resultado no ecrã ou sair 
+		 * se o segundo número for igual a 0
+		 */
+	}
+
+}
